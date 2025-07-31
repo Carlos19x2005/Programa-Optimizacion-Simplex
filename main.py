55b7a3468f13755c37e8d6c1bcc38d9630c89974
Carlos19x2005 <carlosguaillas6@gmail.com>
2025-07-31 08:54:15 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+lp_solver_app/
+│
+├── main.py                 ← Programa principal
+├── parser.py               ← Módulo para extraer variables y restricciones
+├── solver.py               ← Módulo que resuelve con simplex o gráfico
+├── visualizer.py           ← Gráficas para problemas de 2 variables
+├── interpreter.py          ← Genera análisis de resultados
+├── requirements.txt        ← Librerías necesarias
+├── test_cases/             ← Carpeta con ejemplos de prueba
+└── README.md               ← Documentación del proyecto
